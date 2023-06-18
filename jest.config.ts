06f2a4a007e2cb3edcc90af7735bf8a32ff9218d
misong <misong321@gmail.com>
2023-06-18 20:49:53 +0900

    feat: create language selector toggle UI

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,14 @@
 export default {
-  rootDir: '.',
-  roots: ['<rootDir>'],
-  testMatch: ['**/*.test.ts(x)?'],
+  rootDir: ".",
+  roots: ["<rootDir>"],
+  testMatch: ["**/*.test.ts(x)?"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "babel-jest",
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['dotenv/config'],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["dotenv/config"],
   moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
-  }
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
+  // testEnvironment: "jsdom",
 };
--- src/components/templateSelection/TemplateSelector.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const TemplateSelector: React.FC = () => {
-  return (
-    <></>
-  );
-};
-
-export default TemplateSelector;
